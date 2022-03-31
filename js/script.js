e74e6dc3e3d419adc711069db247f4041650a771
Absar Ahmad <absar4060@gmail.com>
2022-03-31 15:30:57 +0530

    responsive website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Code for responsive toggle menu
+
+var navLinks = document.getElementById("nav-links");
+
+function showMenu() {
+    navLinks.style.display = "block";
+}
+
+function hideMenu() {
+    navLinks.style.display = "none";
+}
